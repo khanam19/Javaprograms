f15c0459bcabba9fba99b66617ba055ca241ae72
pkhanam <salmakhanam419@gmail.com>
2025-05-11 20:56:57 +0530

    11 may 2025

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int num =5;
+
+        System.out.printf( "%d * 1 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 2 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 3 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 4 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 5 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 6 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 7 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 8 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 9 = %d%n", num,num * 1 );
+        System.out.printf( "%d * 10 = %d%n", num,num * 1 );
+
+    }
+}
