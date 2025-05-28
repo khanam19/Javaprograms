db0609c62859a6b0737467ef512fc6bba7f21e2f
khanam19 <salmakhanam419@gmail.com>
2025-05-28 17:58:34 +0530

    Add files via upload
    28 May

---
+++ src/EX_10_for_Loop/Lab112_For_loop_Continue.java
@@ -0,0 +1,15 @@
+package EX_10_for_Loop;
+
+public class Lab112_For_loop_Continue {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 50 ; i++) {
+            if(i ==5){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+    }
+}
---
+++ src/Ex_11_while/Lab118_While.java
@@ -0,0 +1,11 @@
+package Ex_11_while;
+
+public class Lab118_While {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+
+    }
+
+}
