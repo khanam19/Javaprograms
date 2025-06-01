6ff6eaac0e26d1b0d1111a739cf05574b6e091a6
khanam19 <salmakhanam419@gmail.com>
2025-06-01 17:30:45 +0530

    Add files via upload

---
+++ src/Lab139_Strings_Interview.java
@@ -0,0 +1,9 @@
+package Ex_14_Strings;
+
+public class Lab139_Strings_Interview {
+    public static void main(String[] args) {
+        String name = "Salma";
+        name = name.toUpperCase();
+        System.out.println(name);
+    }
+}
