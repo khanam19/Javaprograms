9e550111fbc03e34022e7d30d752c5d5e601e228
khanam19 <salmakhanam419@gmail.com>
2025-06-02 20:47:38 +0530

    Delete src/Lab139_Strings_Interview.java

--- src/Lab139_Strings_Interview.java
+++
@@ -1,9 +0,0 @@
-package Ex_14_Strings;
-
-public class Lab139_Strings_Interview {
-    public static void main(String[] args) {
-        String name = "Salma";
-        name = name.toUpperCase();
-        System.out.println(name);
-    }
-}
